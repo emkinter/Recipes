3d7e90da240d0c89454779792e3bea0ded821dfd
Ekkehard <emkinter@me.com>
2022-03-22 09:53:29 -0600

    Initial Commit

---
+++ Shared/Recipe_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_ListApp.swift
+//  Shared
+//
+//  Created by Ekkehard Koch on 2022.03.22.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
