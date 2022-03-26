1030486e83a87b279f60d8bb33ac434b5670452d
Ekkehard <emkinter@me.com>
2022-03-26 06:56:19 -0600

    Added tabview as the starting point of the Recipes App.

---
+++ Shared/View/RecipeTabView.swift
@@ -0,0 +1,39 @@
+//
+//  RecipeTabView.swift
+//  Recipes
+//
+//  Created by Ekkehard Koch on 2022.03.26.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        
+        TabView {
+            
+            Text("Featured View")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
+                }
+            RecipeListView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "list.bullet")
+                        Text("List")
+                    }
+                }
+            
+        }
+        
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
